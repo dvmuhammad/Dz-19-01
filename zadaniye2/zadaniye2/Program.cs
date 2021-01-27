11700004d80f579713ff05ad49d50c537299ce9f
Muhammad <davlatovmuhammad1264@mail.com>
2021-01-27 14:45:29 +0500

    Add zadaniye2/

---
+++ zadaniye2/zadaniye2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace zadaniye2
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			double A = 1.40;
+			double B = 5.50;
+			double C = 0.60;
+			double AC = A - C;
+			double BC = B + C;
+			double result = AC + BC;
+			Console.WriteLine(result);
+			Console.ReadKey();
+		}
+	}
+}
