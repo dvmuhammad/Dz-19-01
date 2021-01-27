17f4fa8d3e7721308d8cea2597e71aaa3d6fe9db
Muhammad <davlatovmuhammad1264@mail.com>
2021-01-27 15:47:13 +0500

    Add zadaniye4/

---
+++ zadaniye4/zadaniye4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace zadaniye4
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int a, b;
+			a = Convert.ToInt16(Console.ReadLine());
+			b = a / 10 + a % 10 * 10;
+			Console.WriteLine(b);
+			Console.ReadKey();
+		}
+	}
+}
