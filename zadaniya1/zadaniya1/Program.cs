25774f2227b4a2659899d414d810a6b232cdfcd4
Muhammad <davlatovmuhammad1264@mail.com>
2021-01-27 14:24:02 +0500

    Add zadaniya1/

---
+++ zadaniya1/zadaniya1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace zadaniya1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			double a = 16.80;
+			double b = 12.40;
+			double c = Math.Sqrt(a * b);
+			Console.WriteLine(c);
+			Console.ReadKey();
+
+		}
+	}
+}
