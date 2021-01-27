401ec17f4ebf07cfd2e2faaa22c85988256521da
Muhammad <davlatovmuhammad1264@mail.com>
2021-01-27 15:48:49 +0500

    Add zadaniya6/

---
+++ zadaniya6/zadaniya6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace zadaniya6
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int k, s, n;
+			n =-1;
+			k = Convert.ToInt32(Console.ReadLine());
+			s = ((k + n) % 7) + 1;
+			Console.WriteLine(s);
+
+			Console.ReadKey();
+
+		}
+	}
+}
