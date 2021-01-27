f582a1e590a56bdacaa3df69746760d5850c6424
Muhammad <davlatovmuhammad1264@mail.com>
2021-01-27 15:48:09 +0500

    Add zadaniya5/

---
+++ zadaniya5/zadaniya5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace zadaniya5
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int N, sec;
+			N = 10985;
+			sec = 60;
+			int result = N / sec;
+			Console.WriteLine(result);
+
+			Console.ReadKey();
+		}
+	}
+}
